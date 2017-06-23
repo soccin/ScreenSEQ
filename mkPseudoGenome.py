ebdf0c9726022cd9a2ca441b76d348597c6e255d
Nicholas D. Socci <soccin@mskcc.org>
2017-06-23 18:59:33 -0400

    Added printout of seq length(s)

--- mkPseudoGenome.py
+++ mkPseudoGenome.py
@@ -4,6 +4,7 @@ import sys
 
 seqLen=0
 sequences=set()
+seqLengths=set()
 print ">"+sys.argv[1]
 for line in sys.stdin:
     if line.startswith(">"):
@@ -14,7 +15,8 @@ for line in sys.stdin:
         continue
     sequences.add(seq)
     seqLen=len(seq)
-
+    seqLengths.add(seqLen)
     print ("n"*(seqLen+1))+seq
 
 print ("n"*seqLen)
+print >>sys.stderr, "Library Seq Length(s)", seqLengths
