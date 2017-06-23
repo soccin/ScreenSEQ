3b8c792f87c34e2dc7b4366fb86484af716ade3b
Nicholas D. Socci <soccin@mskcc.org>
2017-06-23 18:59:11 -0400

    Renamed to findScreenPos and added args

--- findScreenPos.sh
+++ findScreenPos.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-FASTXBIN=./fastx_toolkit-0.0.13
+FASTXBIN=/opt/common/CentOS_6/fastx_toolkit/fastx_toolkit-0.0.13
 GMAPPER=/ifs/work/socci/bin/gmapper-ls
-FASTQ="/ifs/archive/GCL/hiseq/FASTQ/JOHNSAWYERS_0073_000000000-B8L95/Project_07665_B/Sample_Dox3_IGO_07665_B_2/Dox3_IGO_07665_B_2_S5_L001_R1_001.fastq.gz"
 
-SGRNA_LEN=20
+if [ "$#" != "3" ]; then
+    echo usage: findScreenPos.sh SCREEN_GENOME SGRNA_LEN FASTQ
+    exit
+fi
+
+GENOME=$1
+SGRNA_LEN=$2
+FASTQ=$3
 
 zcat $FASTQ \
     | $FASTXBIN/fastq_quality_filter -Q 33 -p 90 -q 30 \
     | $FASTXBIN/fastq_to_fasta -Q33 \
-    | head -100000 \
+    | head -10000 \
     | fgrep -v ">" \
     | ./getSubSeq.py $SGRNA_LEN \
     >test.fasta
 
-$GMAPPER --strata test.fasta human-druggable-top5_pGenome.fa >testmap.sam
+$GMAPPER --strata test.fasta $GENOME >testmap.sam
 cat testmap.sam \
     | fgrep NM:i:0 | fgrep ${SGRNA_LEN}M \
     | cut -f1 | sed 's/.*_//' \
