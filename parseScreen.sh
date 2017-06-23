4655edf9cf4fd29699758c5d90a9673d8dc20571
Nicholas D. Socci <soccin@mskcc.org>
2017-06-23 19:30:12 -0400

    Added args, odir

--- parseScreen.sh
+++ parseScreen.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-FASTQ=$1
 FBIN=/opt/common/CentOS_6/fastx_toolkit/fastx_toolkit-0.0.13
 
-OFILE=$(basename $FASTQ | sed 's/_IGO_.*//').counts
-echo $OFILE
+if [ "$#" != "3" ]; then
+    echo usage: parseScreen.sh START_POS SEQ_LEN FASTQ
+    exit
+fi
 
-zcat $FASTQ \
-    | $FBIN/fastx_trimmer -Q 33 -f 83 -l 102 \
+START_POS=$1
+SEQ_LEN=$2
+END_POS=$((START_POS+SEQ_LEN-1))
+FASTQ=$3
+
+OFILE=$(basename $FASTQ | sed 's/_IGO_.*//').counts
+echo $OFILE, $START_POS, $END_POS
+
+ODIR=LibCounts
+mkdir -p $ODIR
+
+zcat $FASTQ | head -40000 \
+    | $FBIN/fastx_trimmer -Q 33 -f $START_POS -l $END_POS \
     | $FBIN/fastq_quality_converter -Q 33 -n \
     | $FBIN/fastq_quality_filter -Q 33 -p 1 -q 20 \
     | $FBIN/fastx_collapser \
     | $FBIN/fasta_formatter -t \
     | tr '-' '\t' \
     | cut -f2,3 \
-    > $OFILE
+    > $ODIR/$OFILE
