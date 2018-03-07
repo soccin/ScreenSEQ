c741b4d99620c6442e984df200103e0b1ac1ed80
Nicholas D. Socci <soccin@mskcc.org>
2018-03-06 21:32:58 -0500

    Fix countBam to work with different cigars

--- countBAM.sh
+++ countBAM.sh
@@ -6,8 +6,16 @@ sample=$(basename $BAM | sed 's/.bam//' | sed 's/s_//')
 
 mkdir -p Counts
 
+correctCigar=$(samtools view $BAM \
+    | cut -f6 \
+    | perl -ne 'print if m/^\d+M$/' \
+    |  head -10000 \
+    | sort | uniq -c | sort -nr | head -1 | awk '{print $2}')
+
+echo "correctCigar="$correctCigar
+
 samtools view $BAM \
-    | fgrep -w 126M \
+    | fgrep -w $correctCigar \
     | perl -ne  "print if m/NM:i:[01]\s/" \
     | cut -f3 \
     | uniq -c \
