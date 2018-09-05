461699d287ad72e008dac5dc846d65fd6f02058f
Nicholas D. Socci <soccin@mskcc.org>
2018-09-04 23:30:38 -0400

    Added PE versions of count and joinCounts

---
+++ countBAMPE.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BAM=$1
+
+sample=$(basename $BAM | sed 's/.bam//' | sed 's/s_//')
+
+mkdir -p Counts
+
+
+cigarTable=$(
+    samtools view $BAM \
+    | fgrep NM:i:0 \
+    | cut -f2,6 \
+    | head -10000 \
+    | sort \
+    | uniq -c \
+    | sort -nr \
+    | head -2 )
+
+echo "CigarTable ="$cigarTable
+correctCigar=$(echo $cigarTable | awk '{print $3"|"$6}')
+echo $correctCigar
+
+samtools view $BAM \
+    | egrep -w "$correctCigar" \
+    | perl -ne  "print if m/NM:i:[01]\s/" \
+    | cut -f1,3 \
+    | sort -S 4g \
+    | uniq \
+    | cut -f2 \
+    | sort -S 4g \
+    | uniq -c \
+    | awk '{print $2"\t"$1}' \
+    >Counts/counts__${sample}
+
+
+
