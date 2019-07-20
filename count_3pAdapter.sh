853bfaf3fe3edf0daa4f34c77d93f38df8d4edb0
Nicholas D. Socci <soccin@mskcc.org>
2019-07-20 00:36:30 -0400

    Add library paramters to command line

--- count_3pAdapter.sh
+++ count_3pAdapter.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 SBIN=/opt/common/CentOS_6
 
-ADAPTER_3p=GTTTTAGAGCTAGAAATAGCA
-ADAPTER_LEN=21
+# ADAPTER_3p=GTTTTAGAGCTAGAAATAGCA
+# ADAPTER_LEN=21
 
-FASTQ=$1
+ADAPTER_3p=$1
+ADAPTER_LEN=$2
+SGRNA_LEN=$3
+FASTQ=$4
 BASE=$(basename $FASTQ | sed 's/.fastq.gz//')
 
 echo "sgRNA Counts" | tr ' ' '\t' >${BASE}___COUNTS.txt
@@ -12,7 +15,7 @@ echo "sgRNA Counts" | tr ' ' '\t' >${BASE}___COUNTS.txt
 zcat $FASTQ  ${FASTQ/_R1_/_R2_} \
     | $SBIN/cutadapt/cutadapt-1.9.1/bin/cutadapt -a $ADAPTER_3p - -O $ADAPTER_LEN --discard-untrimmed 2> LOG \
     | $SBIN/fastx_toolkit/fastx_toolkit-0.0.13/fastx_reverse_complement -Q 33 \
-    | $SBIN/fastx_toolkit/fastx_toolkit-0.0.13/fastx_trimmer -l 20 -Q 33 \
+    | $SBIN/fastx_toolkit/fastx_toolkit-0.0.13/fastx_trimmer -l $SGRNA_LEN -Q 33 \
     | $SBIN/fastx_toolkit/fastx_toolkit-0.0.13/fastx_reverse_complement -Q 33 \
     | $SBIN/fastx_toolkit/fastx_toolkit-0.0.13/fastq_to_fasta -Q 33 \
     | fgrep -v ">" \
