7969bb4a3fc19672ccf8f71400503e6b0fc60395
Nicholas D. Socci <soccin@mskcc.org>
2021-11-02 17:33:37 -0400

    Start work on MAGeCK version

---
+++ doMAGeCKCounts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# mageck count \
+#     -l InesFM_Inflammation___Lib.txt \
+#     -n p12513 \
+#     --sample-label $(cat LABELS) \
+#     --control-sgrna InesFM_Inflammation___LibControls.txt \
+#     --fastq $(cat FASTQ ) \
+#     --pdf-report
+
